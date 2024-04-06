ea628832dc42e3b114a065bbaa6b3c823edef4e4
AliAli01010101 <at@evamobi.com>
2024-04-06 20:22:50 +0300

    update from 6.04.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,25 +61,11 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom.v20240400))
-    //noinspection UseTomlInstead
-//    implementation("androidx.compose.ui:ui")
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
-    //noinspection UseTomlInstead
-//    implementation("androidx.compose.ui:ui-graphics")
-    //noinspection UseTomlInstead
-//    implementation("androidx.compose.ui:ui-tooling-preview")
-    //noinspection UseTomlInstead
-//    implementation("androidx.compose.material3:material3")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    //noinspection UseTomlInstead
-//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    //noinspection UseTomlInstead
-//    debugImplementation("androidx.compose.ui:ui-tooling")
-    //noinspection UseTomlInstead
-//    debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation (libs.androidx.material)
     implementation(libs.androidx.ads.identifier)
     implementation(libs.guava)
